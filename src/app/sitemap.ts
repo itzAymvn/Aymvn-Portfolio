8b1383d9c9583ca656dc0aa351ba679087964584
Aymvn <aymanbdouzi@gmail.com>
2023-12-14 00:40:34 +0100

    Updated sitemap URLs to include full paths

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "/",
+			url: "https://aymvn.tech/",
 			lastModified: new Date().toISOString(),
 			changeFrequency: "weekly",
 			priority: 1,
 		},
 		{
-			url: "/norage",
+			url: "https://aymvn.tech/norage",
 			lastModified: new Date().toISOString(),
 			changeFrequency: "never",
 			priority: 0.1,
