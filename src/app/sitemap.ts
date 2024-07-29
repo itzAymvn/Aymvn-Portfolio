1cd4c4de6ca0562e2f4f978afd19c5b061565f97
itzAymvn <aymanbdouzi@gmail.com>
2024-07-29 12:45:13 +0100

    chore: Update sitemap URLs to use correct domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "https://aymvn.tech/",
+			url: "https://aymvn.netlify.app/",
 			lastModified: new Date().toISOString(),
-			changeFrequency: "weekly",
+			changeFrequency: "monthly",
 			priority: 1,
 		},
 		{
-			url: "https://aymvn.tech/norage",
+			url: "https://aymvn.netlify.app/norage",
 			lastModified: new Date().toISOString(),
 			changeFrequency: "never",
 			priority: 0.1,
