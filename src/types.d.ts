e58c504a1f3e73adb467004e003002f3c4d8d08a
itzAymvn <aymanbdouzi@gmail.com>
2024-07-29 12:33:46 +0100

    refactor: Update GitHubRepositoryOwner interface in types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,6 +9,27 @@ export interface Project {
 	forked: boolean
 }
 
+export interface GitHubRepositoryOwner {
+    login: string
+    id: number
+    node_id: string
+    avatar_url: string
+    gravatar_id: string
+    url: string
+    html_url: string
+    followers_url: string
+    following_url: string
+    gists_url: string
+    starred_url: string
+    subscriptions_url: string
+    organizations_url: string
+    repos_url: string
+    events_url: string
+    received_events_url: string
+    type: string
+    site_admin: boolean
+}
+
 export interface GitHubRepository {
 	id: number
 	node_id: string
@@ -16,26 +37,7 @@ export interface GitHubRepository {
 	full_name: string
 	private: boolean
 	imgUrl?: string
-	owner: {
-		login: string
-		id: number
-		node_id: string
-		avatar_url: string
-		gravatar_id: string
-		url: string
-		html_url: string
-		followers_url: string
-		following_url: string
-		gists_url: string
-		starred_url: string
-		subscriptions_url: string
-		organizations_url: string
-		repos_url: string
-		events_url: string
-		received_events_url: string
-		type: string
-		site_admin: boolean
-	}
+	owner: GitHubRepositoryOwner
 	html_url: string
 	description: string
 	fork: boolean
