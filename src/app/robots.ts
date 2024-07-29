cfed5542eb71ca257ea669e38857be682ef1a336
itzAymvn <aymanbdouzi@gmail.com>
2024-07-29 12:45:53 +0100

    chore: Update robots to use correct URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
 			userAgent: "*",
 			allow: "/",
 		},
-		sitemap: "https://aymvn.tech/sitemap.xml",
+		sitemap: "https://aymvn.netlify.app/sitemap.xml",
 	}
 }
