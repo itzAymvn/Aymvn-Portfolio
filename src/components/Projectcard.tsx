c64f7ee7a47c449cab1de50bc27d57bf913a62ba
itzAymvn <aymanbdouzi@gmail.com>
2024-07-29 12:51:09 +0100

    chore: Update background image size and position in ProjectCard component

--- src/components/Projectcard.tsx
+++ src/components/Projectcard.tsx
@@ -12,7 +12,8 @@ const ProjectCard = ({ project }: { project: Project }): JSX.Element => {
 				className="h-52 md:h-72 rounded-t-xl relative group"
 				style={{
 					background: `url(${project.imgUrl})`,
-					backgroundSize: "cover",
+					backgroundSize: "contain",
+					backgroundPosition: "center",
 				}}
 			>
 				<div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
